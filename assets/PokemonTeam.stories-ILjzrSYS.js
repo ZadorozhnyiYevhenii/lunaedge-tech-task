b3b50ff60bf579ca6e2a69a986a06710b5240817
Yevhenii <zheki2004@gmail.com>
2023-12-22 11:16:12 +0200

    Updates

---
+++ assets/PokemonTeam.stories-ILjzrSYS.js
@@ -0,0 +1,26 @@
+import"./blocks-i5o0VK9R.js";import{P as i}from"./PokemonTeam-8P5z-RAS.js";import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{useMDXComponents as s}from"./index-_VGcpBFS.js";import{M as a}from"./index-P4MDArQq.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-XY455xTU.js";import"../sb-preview/runtime.js";import"./index-jmm5gWkb.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";function r(o){const n=Object.assign({h1:"h1",h2:"h2",ul:"ul",li:"li",code:"code",strong:"strong",pre:"pre"},s(),o.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"PokemonTeam",component:i}),`
+`,e.jsx(n.h1,{id:"pokemon-team-component",children:"Pokemon Team Component"}),`
+`,e.jsx(n.h2,{id:"additional-information",children:"Additional Information"}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:["The ",e.jsx(n.code,{children:"PokemonTeam"})," component allows you to create a visually appealing Pokemon team display with the following features:"]}),`
+`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Customizable Styles"}),": You can customize the background color, text color, button color, and more."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Interactivity"}),": The component provides interactive elements such as an info button and a remove button for each Pokemon."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"User Interaction Handling"}),": The component exposes callback functions for handling user interactions, such as clicking on the info button or removing a Pokemon."]}),`
+`,e.jsxs(n.li,{children:[e.jsx(n.strong,{children:"Submit Functionality"}),": The ",e.jsx(n.code,{children:"onSubmit"})," function is triggered when the submit button is clicked, providing a way to handle the submission of the Pokemon team."]}),`
+`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"Feel free to explore and tailor the component to fit your specific requirements."}),`
+`]}),`
+`,e.jsx(n.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(n.pre,{children:e.jsx(n.code,{className:"language-jsx",children:`<PokemonTeam
+  selectedPokemons={['Pikachu', 'Bulbasaur', 'Charmander', 'Ivysaur']}
+  isPrimaryButton={true}
+  buttonSize="py-2 px-4"
+  buttonBackgroundColor="blue-500"
+  teamTextColor="black"
+  teamBackgroundColor="white"
+  buttonHoverColor="blue-700"
+/>
+`})})]})}function c(o={}){const{wrapper:n}=Object.assign({},s(),o.components);return n?e.jsx(n,{...o,children:e.jsx(r,{...o})}):r(o)}const l=()=>{throw new Error("Docs-only story")};l.parameters={docsOnly:!0};const t={title:"PokemonTeam",component:i,tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:c};const T=["__page"];export{T as __namedExportsOrder,l as __page,t as default};
---
+++ assets/SelectPokemon.stories-mBCcZqai.js
@@ -0,0 +1,25 @@
+import"./blocks-i5o0VK9R.js";import{S as s}from"./SelectPokemon-d7eQYDLt.js";import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{useMDXComponents as a}from"./index-_VGcpBFS.js";import{M as l}from"./index-P4MDArQq.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./index-jmm5gWkb.js";import"./PokemonTeam-8P5z-RAS.js";import"./iframe-XY455xTU.js";import"../sb-preview/runtime.js";import"./index-ogXoivrg.js";import"./index-MVbLLYTZ.js";import"./index-PPLHz8o0.js";function r(n){const o=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",ul:"ul",li:"li",pre:"pre"},a(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(l,{title:"SelectPokemon",component:s}),`
+`,e.jsx(o.h1,{id:"select-pokemon-component",children:"Select Pokemon Component"}),`
+`,e.jsxs(o.p,{children:["The ",e.jsx(o.code,{children:"SelectPokemon"})," component allows users to select Pokemon and form a team. It provides a dropdown with a search feature, a list of selectable Pokemon, and a display of the selected Pokemon team."]}),`
+`,e.jsx(o.p,{children:`-Fetches a list of Pokemon from an API.
+-Allows users to select up to four Pokemon for their team.
+-Displays selected Pokemon and team details.
+-Provides a search feature to filter Pokemon in the dropdown.`}),`
+`,e.jsx(o.h2,{id:"additional-information",children:"Additional Information"}),`
+`,e.jsxs(o.ul,{children:[`
+`,e.jsx(o.li,{children:"label: The label for the dropdown."}),`
+`,e.jsx(o.li,{children:"backgroundColor: Background color of the component."}),`
+`,e.jsx(o.li,{children:"border: Border color of the dropdown."}),`
+`,e.jsx(o.li,{children:"fontSize: Font size for the text."}),`
+`,e.jsx(o.li,{children:"fontColor: Font color for the text."}),`
+`]}),`
+`,e.jsx(o.h2,{id:"usage",children:"Usage"}),`
+`,e.jsx(o.pre,{children:e.jsx(o.code,{className:"language-jsx",children:`<SelectPokemon
+  label="Select Pokemon Team"
+  backgroundColor="white"
+  border="gray-300"
+  fontSize="text-base"
+  fontColor="text-black"
+  padding="p-4"
+/>
+`})})]})}function i(n={}){const{wrapper:o}=Object.assign({},a(),n.components);return o?e.jsx(o,{...n,children:e.jsx(r,{...n})}):r(n)}const c=()=>{throw new Error("Docs-only story")};c.parameters={docsOnly:!0};const t={title:"SelectPokemon",component:s,tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:i};const _=["__page"];export{_ as __namedExportsOrder,c as __page,t as default};
